465e1e785a20f933be402c365bc984ba8c8d6c5a
Aedriel Kyle Javier <43.splash@gmail.com>
2021-09-19 16:36:16 -0400

    added new settings in xinput.sh

--- awesome/xinput.sh
+++ awesome/xinput.sh
@@ -2,4 +2,5 @@
 
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Circular Scrolling" 0
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Edge Scrolling" 0, 0, 0
+xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Profile" 1
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Constant Deceleration" 2.000000
