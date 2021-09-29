4fe401e765f360cc45e3c2569cf7890e85b265c4
Aedriel Kyle Javier <43.splash@gmail.com>
2021-09-28 21:06:25 -0400

    changed some settings and updated installed package list

--- awesome/xinput-daemon.sh
+++ awesome/xinput-daemon.sh
@@ -4,6 +4,6 @@ xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Circular Scrolling" 0
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Edge Scrolling" 0, 0, 0
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Profile" 1
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Constant Deceleration" 2.000000
-xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" 0.100000
+xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" 0.000000
 
 while true; do state=$(lsusb) && sleep 2 && [[ $state != $(lsusb) ]] && /home/kylert/.config/awesome/xinput.sh; done
--- awesome/xinput.sh
+++ awesome/xinput.sh
@@ -5,4 +5,4 @@ xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Circular Scrolling" 0
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Edge Scrolling" 0, 0, 0
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Profile" 1
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Constant Deceleration" 2.000000
-xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" 0.100000
+xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" 0.000000
