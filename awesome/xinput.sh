176f5fc5b01d082b56d4a4dea74edda9ffe4f46e
Aedriel Kyle Javier <43.splash@gmail.com>
2021-11-04 02:53:44 -0400

    changed some peripheral settings

--- awesome/xinput.sh
+++ awesome/xinput.sh
@@ -5,5 +5,5 @@ xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Circular Scrolling" 0
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Edge Scrolling" 0, 0, 0
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Profile" 1
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Constant Deceleration" 2.000000
-xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" -0.650000
+xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" 0.300000
 xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Scrolling Pixel Distance" 50
