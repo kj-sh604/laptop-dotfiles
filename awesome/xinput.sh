77b36025bc079245b3f68ba91fb80ba31631beca
Aedriel Kyle Javier <43.splash@gmail.com>
2021-11-11 20:12:15 -0500

    modified trackpoint scroll behavior

--- awesome/xinput.sh
+++ awesome/xinput.sh
@@ -7,3 +7,4 @@ xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Profile" 1
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Constant Deceleration" 2.000000
 xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" 0.300000
 xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Scrolling Pixel Distance" 50
+xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "libinput Scrolling Pixel Distance" 10
