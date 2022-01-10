714b5db79ff38466ca5c8540183538b63f32e3d4
Aedriel Kyle Javier <43.splash@gmail.com>
2022-01-10 07:31:09 -0500

    disabled trackpad just for fun & to get better at trackpoint

--- awesome/xinput-daemon.sh
+++ awesome/xinput-daemon.sh
@@ -4,6 +4,7 @@ xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Circular Scrolling" 0
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Edge Scrolling" 0, 0, 0
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Profile" 1
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Constant Deceleration" 2.000000
+xinput set-prop pointer:"SynPS/2 Synaptics TouchPad" "Device Enabled" 0
 xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" 0.300000
 xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "libinput Scrolling Pixel Distance" 10
 
