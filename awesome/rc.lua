60b59e6d3070bb934f48905e9a30aa582075f5c1
Aedriel Kyle Javier <43.splash@gmail.com>
2022-04-05 15:00:24 -0400

    fixed zoom meetings that are started from a browser instance

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -55,7 +55,7 @@ awful.layout.layouts = {
 -- awful.layout.suit.corner.ne,
 -- awful.layout.suit.corner.sw,
 -- awful.layout.suit.corner.se,
-  awful.layout.suit.floating,
+-- awful.layout.suit.floating,
 }
 -- }}}
 
@@ -575,11 +575,11 @@ awful.rules.rules = {
     }
   },
 
-  -- Floating clients.
+-- Floating clients.
   { rule_any = {
     instance = {
-      "DTA", -- Firefox addon DownThemAll.
-      "copyq", -- Includes session name in class.
+      "DTA",  -- Firefox addon DownThemAll.
+      "copyq",  -- Includes session name in class.
       "pinentry",
     },
     class = {
@@ -589,29 +589,29 @@ awful.rules.rules = {
       "Gnome-calculator",
       "Gnome-system-monitor",
       "Kruler",
-      "MessageWin", -- kalarm.
+      "MessageWin",  -- kalarm.
+      "screengrab",
       "Sxiv",
       "Steam",
       "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
       "Wpa_gui",
       "veromix",
       "xtightvncviewer",
-      "zoom" },
+      "zoom",
+      "[join?]*",
+      "[start?]*",
+    },
 
     -- Note that the name property shown in xprop might be set slightly after creation of the client
     -- and the name shown there might not match defined rules here.
     name = {
-      "Event Tester", -- xev.
-      "Task Manager",
-      "Zoom - Free Account",
-      "Zoom - Licensed Account",
-      "Zoom Meeting",
-      "zoom", 
+      "Event Tester",  -- xev.
+      "Task Manager",      
     },
     role = {
-      "AlarmWindow", -- Thunderbird's calendar.
-      "ConfigManager", -- Thunderbird's about:config.
-      "pop-up", -- e.g. Google Chrome's (detached) Developer Tools.
+      "AlarmWindow",  -- Thunderbird's calendar.
+      "ConfigManager",  -- Thunderbird's about:config.
+      "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
     }
   }, properties = { floating = true }},
 
