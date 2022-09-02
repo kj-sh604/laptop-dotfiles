bd0249d87b4fb915e6e7c0e17e50f9753de20029
Aedriel Kyle Javier <43.splash@gmail.com>
2022-09-01 20:36:14 -0400

    refactor: added pomodoro aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,8 @@ if status is-interactive
     alias s="cd ~/.local/share/scripts && lsd --group-dirs first -h --icon-theme unicode -L"
     alias d="disown"
     alias c="cal"
+    alias work="timer 30m && notify-send 'Pomodoro' 'Work Timer is up! Take a Break 😊' -i '/home/kylert/.cache/pomo/pomo-tomato.png' -t 30000 -w -A 'Dismiss' & disown; mpv '/home/kylert/.cache/pomo/pomo-sound.mp3'"
+    alias rest="timer 10m && notify-send 'Pomodoro' 'Break is over! Get back to work 😬' -i '/home/kylert/.cache/pomo/pomo-tomato.png' -t 30000 -w -A 'Dismiss' & disown; mpv '/home/kylert/.cache/pomo/pomo-sound.mp3'"
     export PF_INFO="ascii title os kernel uptime pkgs memory"
 
 
