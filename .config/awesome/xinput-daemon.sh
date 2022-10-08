692bce250b0cd76280c6ef8334df44166f0a36b0
Aedriel Kyle Javier <43.splash@gmail.com>
2022-10-08 07:13:14 -0400

    refactor: updated peripherals for t480

--- .config/awesome/xinput-daemon.sh
+++ .config/awesome/xinput-daemon.sh
@@ -3,14 +3,11 @@
 apply_settings () {
 
   sleep 2
-  xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Circular Scrolling" 0
-  xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Edge Scrolling" 0, 0, 0
-  xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Profile" 1
-  xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Constant Deceleration" 2.000000
-  xinput set-prop pointer:"SynPS/2 Synaptics TouchPad" "Device Enabled" 0
+  xinput set-prop pointer:"Synaptics TM3276-022" "Device Enabled" 0
+  xinput set-prop pointer:"G2Touch Multi-Touch by G2TSP" "Device Enabled" 0
   xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" 0.300000
   xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "libinput Scrolling Pixel Distance" 10
-
+  xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "libinput Accel Speed" 1.000000
 }
 
 apply_settings
