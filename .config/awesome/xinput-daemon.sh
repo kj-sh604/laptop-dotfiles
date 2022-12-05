525a7079f27ed066f8dad5b614fa962a646ab7cf
Aedriel Kyle Javier <43.splash@gmail.com>
2022-12-05 17:55:11 -0500

    refactor: swiched to legacy evdev driver for trackpoint, hence new setting parameters

--- .config/awesome/xinput-daemon.sh
+++ .config/awesome/xinput-daemon.sh
@@ -6,7 +6,11 @@ apply_settings () {
   xinput set-prop pointer:"Synaptics TM3276-022" "Device Enabled" 0
   xinput set-prop pointer:"G2Touch Multi-Touch by G2TSP" "Device Enabled" 0
   xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" 0.300000
-  xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "libinput Scrolling Pixel Distance" 10
+  xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "Device Accel Profile" 4
+  xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "Device Accel Constant Deceleration" 0.400000
+
+  ## libinput Trackpoint Settings
+  # xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "libinput Scrolling Pixel Distance" 10
   # xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "libinput Accel Speed" 0.800000
 }
 
