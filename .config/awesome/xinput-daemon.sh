986748438554c5fd6a2b56d6a41d78d3a1f26c73
Aedriel Kyle Javier <43.splash@gmail.com>
2022-12-11 20:25:02 -0500

    refactor: reverted back to legacy evdev for trackpoint

--- .config/awesome/xinput-daemon.sh
+++ .config/awesome/xinput-daemon.sh
@@ -8,12 +8,12 @@ apply_settings () {
   xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" 0.300000
 
   ## evdev Trackpoint settings when not using X11 Option "Resolution" for pointer speed
-  # xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "Device Accel Profile" 2
+  xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "Device Accel Profile" 2
   # xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "Device Accel Constant Deceleration" 0.450000
 
   ## libinput Trackpoint Settings
-  xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "libinput Scrolling Pixel Distance" 10
-  xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "libinput Accel Speed" 0.800000
+  # xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "libinput Scrolling Pixel Distance" 10
+  # xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "libinput Accel Speed" 0.800000
 }
 
 apply_settings
