afa9888f8c3a23f8bc574f2c5f35e1cb63f7d818
Aedriel Kyle Javier <43.splash@gmail.com>
2023-04-13 22:33:59 -0400

    refactor: made screen layout more robust and dynamic

--- .config/awesome/default-screen-layout.sh
+++ .config/awesome/default-screen-layout.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
+LAPTOP_SCREEN="eDP1"
+
+# disable all connected screens except for the laptop screen
+for SCREEN in $(xrandr | grep "connected" | awk '{ print$1 }' | grep -v "$LAPTOP_SCREEN"); do
+    xrandr --output "$SCREEN" --off
+done
+
+# set the laptop screen as primary and adjust its position
+xrandr --output "$LAPTOP_SCREEN" --primary --auto --pos 0x0 --rotate normal
+
+# reset any panning settings that might have been applied to the laptop screen
+xrandr --output "$LAPTOP_SCREEN" --panning 0x0
+
+# if above does not work run the following command manually:
+# xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP1-1 --off --output DP1-2 --off --output DP1-3 --off --output DP2 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
