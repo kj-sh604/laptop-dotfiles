acb5da16f2ee785d091fce9df8114b3a69dca96d
kj_sh604 <43.splash@gmail.com>
2023-06-16 14:08:43 -0400

    refactor: got into an argument with my girlfriend about tabs over spaces… and then I fell down the stairs

--- .config/awesome/xinput-daemon.sh
+++ .config/awesome/xinput-daemon.sh
@@ -2,16 +2,16 @@
 
 # Function to apply the desired settings
 apply_settings() {
-	sleep 2
-	xinput set-prop pointer:"Synaptics TM3276-022" "Device Enabled" 0
-	xinput set-prop pointer:"G2Touch Multi-Touch by G2TSP" "Device Enabled" 0
-	xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" 0.300000
-	xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "libinput Scrolling Pixel Distance" 10
-	xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "libinput Accel Speed" 0.800000
-
-	### Alternate TrackPoint settings using evdev ###
-	# xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "Device Accel Profile" 2
-	# xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "Device Accel Constant Deceleration" 0.450000
+    sleep 2
+    xinput set-prop pointer:"Synaptics TM3276-022" "Device Enabled" 0
+    xinput set-prop pointer:"G2Touch Multi-Touch by G2TSP" "Device Enabled" 0
+    xinput set-prop pointer:"ELECOM ELECOM TrackBall Mouse" "libinput Accel Speed" 0.300000
+    xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "libinput Scrolling Pixel Distance" 10
+    xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "libinput Accel Speed" 0.800000
+    
+    ### Alternate TrackPoint settings using evdev ###
+    # xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "Device Accel Profile" 2
+    # xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "Device Accel Constant Deceleration" 0.450000
 }
 
 # Initial application of settings
@@ -19,15 +19,15 @@ apply_settings
 
 # Continuously monitor for changes in USB devices and reapply settings if any change is detected
 while true; do
-	# Get the current state of USB devices
-	state=$(lsusb)
-
-	# Wait for 2 seconds
-	sleep 2
-
-	# Compare the current state with the new state of USB devices
-	# If any change is detected, reapply the settings
-	if [ "$state" != "$(lsusb)" ]; then
-		apply_settings
-	fi
+    # Get the current state of USB devices
+    state=$(lsusb)
+    
+    # Wait for 2 seconds
+    sleep 2
+    
+    # Compare the current state with the new state of USB devices
+    # If any change is detected, reapply the settings
+    if [ "$state" != "$(lsusb)" ]; then
+    	apply_settings
+    fi
 done
