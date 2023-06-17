7987a323f7272e3ce30838f9017f4b54dc2902d1
kj_sh604 <43.splash@gmail.com>
2023-06-17 00:50:16 -0400

    refactor: removed descriptive comments, readable as-is

--- .config/awesome/xinput-daemon.sh
+++ .config/awesome/xinput-daemon.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# Function to apply the desired settings
 apply_settings() {
     sleep 2
     xinput set-prop pointer:"Synaptics TM3276-022" "Device Enabled" 0
@@ -14,19 +13,12 @@ apply_settings() {
     # xinput set-prop pointer:"TPPS/2 IBM TrackPoint" "Device Accel Constant Deceleration" 0.450000
 }
 
-# Initial application of settings
 apply_settings
 
-# Continuously monitor for changes in USB devices and reapply settings if any change is detected
 while true; do
-    # Get the current state of USB devices
     state=$(lsusb)
-    
-    # Wait for 2 seconds
     sleep 2
     
-    # Compare the current state with the new state of USB devices
-    # If any change is detected, reapply the settings
     if [ "$state" != "$(lsusb)" ]; then
     	apply_settings
     fi
