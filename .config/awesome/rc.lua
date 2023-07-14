ba594eb461cf3d3a2369fdb15d0c935166cedcfd
kj_sh604 <43.splash@gmail.com>
2023-07-14 07:10:07 -0400

    refactor: changes related to smaller `wibar`

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -241,9 +241,7 @@ awful.screen.connect_for_each_screen(function(s)
   }
 
   -- Create the wibox
-  s.mywibox = awful.wibar({ position = "top", screen = s, height = 22 })
-  -- use `height = 20` for 80 dpi
-  -- use `height = 22` for 96 dpi
+  s.mywibox = awful.wibar({ position = "top", screen = s, height = 20 })
 
   -- Add widgets to the wibox
   s.mywibox:setup {
