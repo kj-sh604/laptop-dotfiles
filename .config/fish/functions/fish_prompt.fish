92bd4c8aeb47b3508179d01be1444a3944dea389
kj_sh604 <43.splash@gmail.com>
2023-08-04 09:13:45 -0400

    refactor: aligned with `kj_sh604/dotfiles`

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 set -g __fish_git_prompt_show_informative_status 1
-set -g __fish_git_prompt_hide_untrackedfiles 1
+set -g __fish_git_prompt_showuntrackedfiles 1
 set -g __fish_git_prompt_showcolorhints 1
 
 set -g __fish_git_prompt_color_branch magenta --bold
@@ -10,14 +10,14 @@ set -g __fish_git_prompt_char_upstream_prefix ""
 
 set -g __fish_git_prompt_char_stagedstate "•"
 set -g __fish_git_prompt_char_dirtystate "+"
-set -g __fish_git_prompt_char_untrackedfiles "…"
+set -g __fish_git_prompt_char_untrackedfiles "U"
 set -g __fish_git_prompt_char_conflictedstate "x"
 set -g __fish_git_prompt_char_cleanstate "✓"
 
 set -g __fish_git_prompt_color_dirtystate blue
 set -g __fish_git_prompt_color_stagedstate yellow
 set -g __fish_git_prompt_color_invalidstate red
-set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set -g __fish_git_prompt_color_untrackedfiles blue --bold
 set -g __fish_git_prompt_color_cleanstate green --bold
 
 function fish_prompt
